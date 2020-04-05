0289a833d5942e4563cd62e7469fa920905b788d
Ana Caroline Bayma Meyer <abaymame@risd.edu>
2020-04-05 16:58:15 -0400

    garethjonesproject1ABM

---
+++ index.js
@@ -0,0 +1,8 @@
+$('.comp').on('click', function() {
+	$(this).find('.comp-screen').toggleClass('comp-screen-closed');
+	$('.comp-top').toggleClass('comp-top-closed');
+});
+
+//function myFunction() {
+//  document.getElementById("text").find(".comp-screen").toggleClass("comp-screen-closed");(".comp-top").toggleClass("comp-top-closed");
+//}
